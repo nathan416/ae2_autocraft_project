cdb69ea6ab32d152a6d2d3d99700347d2dbd9bf1
nathan416 <30889482+nathan416@users.noreply.github.com>
2018-08-08 00:03:04 -0700

    added file

---
+++ example2.lua
@@ -0,0 +1,25 @@
+require("autocraft_library")
+
+local AUTOCRAFTABLES =
+{
+["Certus Quartz Crystal"] = 5000,
+["Leather"] = 300,
+["Diamond"] = 10000,
+["Emerald"] = 10000,
+["Redstone"] = 10000,
+["Glowstone Dust"] = 10000,
+["Lapis Lazuli"] = 10000,
+["Gold Nugget"] = 10000,
+["Blaze Powder"] = 10000,
+["Blizz Powder"] = 10000,
+["Fluix Crystal"] = 1000,
+["Nether Quartz"] = 30000,
+["Pure Certus Quartz Crystal"] = 1000,
+["Pure Fluix Crystal"] = 1000,
+["Ender Pearl"] = 1000,
+["Glass"] = 10000,
+["Stygian Iron Plate"] = 1000,
+}
+
+
+autoCraft(AUTOCRAFTABLES)
